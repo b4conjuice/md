c678befc40f1470612eb080715139d87aff8a05e
Daniel Lopez <dlopez807@gmail.com>
2024-02-29 16:16:41 -0800

    add static site generation to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,31 +2,28 @@ import { Main } from '@bacondotbuild/ui'
 
 import Layout from '@/components/layout'
 import Markdown from '@/components/markdown'
-import Loading from '@/components/loading'
-import { api } from '@/utils/api'
+import { generateSSGHelper } from '@/utils/ssgHelper'
 
-export default function Home() {
-  const { data: note, isLoading } = api.notes.getHome.useQuery()
-
-  if (isLoading || !note)
-    return (
-      <Layout>
-        <Main className='flex flex-col p-4'>
-          <div className='flex flex-grow flex-col items-center justify-center space-y-4'>
-            <Loading />
-          </div>
-        </Main>
-      </Layout>
-    )
+export default function Home({ markdown }: { markdown: string }) {
   return (
     <Layout>
       <Main className='flex flex-col p-4'>
         <div className='flex flex-grow flex-col items-center justify-center space-y-4'>
-          <Markdown content={note.markdown ?? ''} />
+          <Markdown content={markdown} />
         </div>
       </Main>
     </Layout>
   )
 }
 
-// server side
+export const getStaticProps = async () => {
+  const ssg = generateSSGHelper()
+
+  const note = await ssg.notes.getHome.fetch()
+  const markdown = note?.markdown
+
+  return {
+    props: { markdown },
+    revalidate: 1,
+  }
+}
